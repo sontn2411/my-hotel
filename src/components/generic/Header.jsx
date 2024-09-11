6c65d61df99c47b13cd4fba318f60e7c605e34d7
unknown <sontn2411@gmail.com>
2024-09-11 13:25:16 +0700

    fix : add boostrap

--- src/components/generic/Header.jsx
+++ src/components/generic/Header.jsx
@@ -1,50 +1,41 @@
-import { Link } from 'react-router-dom'
-import '../../scss/generic/header.scss';
-import iconCall from '../../assets/icons/call.png';
-import iconCallHover from '../../assets/icons/callHover.png'
-import logo from '../../assets/logo.svg';
-import { useState } from 'react';
-
-
+import { Link } from "react-router-dom";
+import "../../scss/generic/header.scss";
+import iconCall from "../../assets/icons/call.png";
+import iconCallHover from "../../assets/icons/callHover.png";
+import logo from "../../assets/logo.svg";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
-
-    return (
-        <div className="wrapperHeader">
-            <div className="headerContainer">
-
-                {/* logo */}
-                <div className="headerLogo">
-                    <Link to='/'>
-                        <h3>Logo</h3>
-                    </Link>
-                </div>
-
-                {/* menu */}
-                <div className="headerMenu">
-                    <ul>
-                        <li>
-                            <Link to='/about'>Giới thiệu</Link>
-                        </li>
-                        <li>
-                            <Link>Phòng</Link>
-                        </li>
-                        <li>
-                            <Link>Ưu đãi</Link>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* call */}
-                <div className="headerCall">
-                    <a href='tel:02581234456'>
-                        <img src={iconCallHover} alt="call" width='20px' /> 
-                        <span>+84 258 1 234 456 </span>
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className="wrapperHeader">
+      <div className="headerContainer">
+        {/* logo */}
+        <div className="headerLogo">
+          <Link to="/">
+            <h3>Logo</h3>
+          </Link>
         </div>
-    )
-}
 
-export default Header
+        {/* menu */}
+        <div className="headerMenu">
+          <ul>
+            <li>Giới thiệu</li>
+            <li>Phòng</li>
+            <li>Ưu đãi</li>
+          </ul>
+        </div>
+
+        {/* call */}
+        <div className="headerCall">
+          <a href="tel:02581234456">
+            <img src={iconCallHover} alt="call" width="20px" />
+            <span>+84 258 1 234 456 </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
