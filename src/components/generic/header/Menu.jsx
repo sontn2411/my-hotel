453a4ad4caa2246901154cb457a9477c8eca9c1b
unknown <sontn2411@gmail.com>
2024-09-13 15:00:53 +0700

    update sub menu

--- src/components/generic/header/Menu.jsx
+++ src/components/generic/header/Menu.jsx
@@ -1,70 +1,71 @@
 import { homeIcon } from "@/assets";
 import MenuItems from "./MenuItems";
-import { contactIcon, experienceIcon, foodIcon, promotionIcon, roomIcon } from "@/assets";
-import { conferenceIcon, utilitieIcon } from "../../../assets";
+import {
+  contactIcon,
+  experienceIcon,
+  foodIcon,
+  promotionIcon,
+  roomIcon,
+  conferenceIcon, utilitieIcon
+} from "@/assets";
 
 
 const menuDataItems = [
   {
-    id : 1 ,
-    name : "Trang chủ",
-    icon : homeIcon ,
-    childrens : [
-      {id : 1.1 , name : "Home 1" , childrens : [
-        {id : 1.11, name : "home 1.1"}
-      ]} ,
-      {id : 1.2 , name : "home 2" }
-    ]
-  } ,
-  {
-    id:2 ,
-    name : 'Phòng nghỉ',
-    icon : roomIcon ,
-    childrens : []
+    id: 1,
+    name: "Trang chủ",
+    icon: homeIcon,
+    childrens: [
+      { id: 1.1, name: "Home 1", childrens: [{ id: 1.11, name: "home 1.1" }] },
+      { id: 1.2, name: "home 2" },
+    ],
   },
   {
-    id: 3, 
-    name : "Ẩm thực",
-    icon : foodIcon
+    id: 2,
+    name: "Phòng nghỉ",
+    icon: roomIcon,
+    childrens: [],
   },
   {
-    id: 4, 
-    name : "Hội nghị & sự kiện" ,
-    icon : conferenceIcon
+    id: 3,
+    name: "Ẩm thực",
+    icon: foodIcon,
   },
   {
-    id: 5 ,
-    name : "Tiện ích" ,
-    icon : utilitieIcon,
+    id: 4,
+    name: "Hội nghị & sự kiện",
+    icon: conferenceIcon,
   },
   {
-    id : 6 ,
-    name : "Ưu đãi", 
-    icon : promotionIcon
+    id: 5,
+    name: "Tiện ích",
+    icon: utilitieIcon,
   },
   {
-    id:7 , 
-    name : "Trải nghiệm",
-    icon : experienceIcon
+    id: 6,
+    name: "Ưu đãi",
+    icon: promotionIcon,
   },
   {
-    id : 8 ,
-    name : "Liên hệ", 
-    icon : contactIcon
-  }
-]
-
+    id: 7,
+    name: "Trải nghiệm",
+    icon: experienceIcon,
+  },
+  {
+    id: 8,
+    name: "Liên hệ",
+    icon: contactIcon,
+  },
+];
 
 const Menu = () => {
   return (
     <div className="container containerHeader ">
-         <ul className="listMenuHeader">
-        {menuDataItems.map(item =>{
-          return (
-            <MenuItems key={item.id} item={item} />
-          )
+      <ul className="listMenuHeader">
+        {menuDataItems.map((item) => {
+          return <MenuItems key={item.id} item={item} />;
         })}
-    </ul>
+      </ul>
     </div>
   );
 };
